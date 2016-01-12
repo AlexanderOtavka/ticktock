f47d5c0b99aa639736bd08b944f0d1a5abcdd666
Zander Otavka <zotavka@gmail.com>
2016-01-11 19:50:30 -0800

    Remove redirect.py.

--- api/__init__.py
+++ api/__init__.py
@@ -7,7 +7,6 @@ import endpoints
 from calendarsapi import CalendarsAPI
 from eventsapi import EventsAPI
 from publicapi import PublicAPI
-from redirect import explorer_redirect
 
 __author__ = "Alexander Otavka"
 __copyright__ = "Copyright (C) 2015 DHS Developers Club"
