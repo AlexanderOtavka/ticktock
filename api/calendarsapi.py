d97b05306668df6daab42ddc1471932f6ec5e377
Zander Otavka <zotavka@gmail.com>
2016-01-13 18:17:40 -0800

    The calenders api manages all of a user's calendars.
    There is no longer a concept of adding or deleting.

--- api/calendarsapi.py
+++ api/calendarsapi.py
@@ -2,12 +2,9 @@
 
 from __future__ import division, print_function
 
-import logging
-
 import endpoints
 from google.appengine.ext import ndb
-from protorpc import remote, message_types
-from oauth2client.appengine import AppAssertionCredentials
+from protorpc import remote
 
 from ticktockapi import ticktock_api
 import messages
@@ -15,7 +12,6 @@ import models
 import authutils
 import gapiutils
 import searchutils
-import strings
 
 __author__ = "Alexander Otavka"
 __copyright__ = "Copyright (C) 2015 DHS Developers Club"
@@ -40,41 +36,39 @@ class CalendarsAPI(remote.Service):
         # Get list of all calendars available to the user
         user_service = authutils.get_service(authutils.CALENDAR_API_NAME,
                                              authutils.CALENDAR_API_VERSION)
-        app_service = authutils.get_service(
-                authutils.CALENDAR_API_NAME,
-                authutils.CALENDAR_API_VERSION,
-                AppAssertionCredentials(authutils.SERVICE_ACCOUNT_SCOPES))
-        all_calendars = (gapiutils.get_calendars(user_service) +
-                         gapiutils.get_calendars(app_service))
+        # app_service = authutils.get_service(
+        #         authutils.CALENDAR_API_NAME,
+        #         authutils.CALENDAR_API_VERSION,
+        #         AppAssertionCredentials(authutils.SERVICE_ACCOUNT_SCOPES))
+        all_calendars = (gapiutils.get_calendars(user_service))
+        # gapiutils.get_calendars(app_service))
 
-        # Filter out calendars not added in user's ndb
+        calendars = []
+
+        # Update calendars from NDB and filter out wrong states
         user_key = models.get_user_key(user_id)
-        chosen_ndb = models.Calendar.query(ancestor=user_key)
-        if request.hidden is not None:
-            chosen_ndb.filter(models.Calendar.hidden == request.hidden)
-        chosen_calendars = []
-        for entity in chosen_ndb.fetch():
-            for cal in all_calendars:
-                if cal.calendarId == entity.key.string_id():
-                    cal.hidden = entity.hidden
-                    if cal.hidden is None:
-                        cal.hidden = False
-                    chosen_calendars.append(cal)
-                    break
-            else:
-                logging.info(strings.logging_delete_unbound_calendar(
-                        calendar_id=entity.key.string_id(), user_id=user_id))
-                entity.key.delete()
+        for calendar in all_calendars:
+            entity = ndb.Key(models.Calendar, calendar.calendarId,
+                             parent=user_key).get()
+            if entity is not None:
+                calendar.hidden = entity.hidden
+            if calendar.hidden is None:
+                calendar.hidden = True
+
+            if request.hidden is not None:
+                if calendar.hidden != request.hidden:
+                    continue
+
+            calendars.append(calendar)
 
         # Sort and search
-        search = request.search
-        if search:
-            chosen_calendars = searchutils.calendar_keyword_alpha_search(
-                chosen_calendars, search)
+        if request.search:
+            calendars = searchutils.calendar_keyword_alpha_search(
+                calendars, request.search)
         else:
-            chosen_calendars = searchutils.calendar_alpha_sort(chosen_calendars)
+            calendars = searchutils.calendar_alpha_sort(calendars)
 
-        return messages.CalendarCollection(items=chosen_calendars)
+        return messages.CalendarCollection(items=calendars)
 
     @endpoints.method(messages.CALENDAR_ID_RESOURCE,
                       messages.CalendarProperties,
@@ -94,12 +88,14 @@ class CalendarsAPI(remote.Service):
         user_key = models.get_user_key(user_id)
         entity = ndb.Key(models.Calendar, request.calendarId,
                          parent=user_key).get()
-        if entity is None:
-            raise endpoints.NotFoundException(
-                    strings.error_calendar_not_added(
-                            calendar_id=request.calendarId))
+        if entity is not None:
+            calendar.hidden = entity.hidden
 
-        calendar.hidden = entity.hidden
+        # if entity is None:
+        #     raise endpoints.NotFoundException(
+        #             strings.error_calendar_not_added(
+        #                     calendar_id=request.calendarId))
+
         if calendar.hidden is None:
             calendar.hidden = False
 
@@ -166,35 +162,37 @@ class CalendarsAPI(remote.Service):
         entity.put()
         return messages.CalendarWriteProperties(hidden=entity.hidden)
 
-    @endpoints.method(messages.CALENDAR_ID_RESOURCE, message_types.VoidMessage,
-                      http_method="DELETE", path="{calendarId}")
-    def delete(self, request):
-        """
-        Remove a calendar from a user's list.
-
-        :type request: messages.CALENDAR_ID_RESOURCE
-        """
-        user_id = authutils.require_user_id()
-
-        # Validate calendar's existence
-        service = authutils.get_service(authutils.CALENDAR_API_NAME,
-                                        authutils.CALENDAR_API_VERSION)
-        gapiutils.get_calendar(service, request.calendarId,
-                               validation_only=True)
-
-        # Get key from request.calendarId and delete it.
-        user_key = models.get_user_key(user_id)
-        key = ndb.Key(models.Calendar, request.calendarId, parent=user_key)
-        if key.get() is None:
-            raise endpoints.NotFoundException(
-                    strings.error_calendar_not_added(
-                            calendar_id=request.calendarId))
-        key.delete()
-
-        # Does not delete the calendar's events' data, just in case the user
-        # wants to undo, they can re-add the calendar, and have all of their
-        # starred and hidden events remain.  I'm still not sure if that's the
-        # best behavior, but I'm going to err on the side of least destruction,
-        # when it comes to data loss.
-
-        return message_types.VoidMessage()
+    # @endpoints.method(messages.CALENDAR_ID_RESOURCE,
+        # message_types.VoidMessage,
+    #                   http_method="DELETE", path="{calendarId}")
+    # def delete(self, request):
+    #     """
+    #     Remove a calendar from a user's list.
+    #
+    #     :type request: messages.CALENDAR_ID_RESOURCE
+    #     """
+    #     user_id = authutils.require_user_id()
+    #
+    #     # Validate calendar's existence
+    #     service = authutils.get_service(authutils.CALENDAR_API_NAME,
+    #                                     authutils.CALENDAR_API_VERSION)
+    #     gapiutils.get_calendar(service, request.calendarId,
+    #                            validation_only=True)
+    #
+    #     # Get key from request.calendarId and delete it.
+    #     user_key = models.get_user_key(user_id)
+    #     key = ndb.Key(models.Calendar, request.calendarId, parent=user_key)
+    #     if key.get() is None:
+    #         raise endpoints.NotFoundException(
+    #                 strings.error_calendar_not_added(
+    #                         calendar_id=request.calendarId))
+    #     key.delete()
+    #
+    #     # Does not delete the calendar's events' data, just in case the user
+    #     # wants to undo, they can re-add the calendar, and have all of their
+    #     # starred and hidden events remain.  I'm still not sure if that's the
+    #     # best behavior, but I'm going to err on the side of least
+        # destruction,
+    #     # when it comes to data loss.
+    #
+    #     return message_types.VoidMessage()
